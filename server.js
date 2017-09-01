867449ed02d80c5db29e9afa6330dfddbc65afc7
James Langford <derivative7117@live.com>
2017-08-31 21:13:48 -0600

    added resume

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express'),
     app = express(),
     port = process.env.PORT || 3099;
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
+
+//<------ production --------->
 
 process.env.PWD = process.cwd();
 
 app.use('/', express.static(process.env.PWD + '/dist'));
 
+//<--------------------------->
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
