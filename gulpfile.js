30083e751af820b2f90d39a7d03beb23df3167b7
James Langford <derivative7117@live.com>
2017-09-05 23:43:02 -0600

    updated everything

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,11 @@ const gulp = require('gulp'),
 
 // Set the banner content
 var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  ''
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+    ' */\n',
+    ''
 ].join('');
 
 // Compiles SCSS files from /scss into /css
@@ -24,56 +24,52 @@ var banner = ['/*!\n',
 // });
 
 // Minify compiled CSS
-gulp.task('minify-css', function() {
-  return gulp.src('./client/css/*.css')
-    .pipe($.cleanCss({
-      compatibility: 'ie8'
-    }))
-    .pipe($.rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('dist/css'))
+gulp.task('minify-css', function () {
+    return gulp.src('./client/css/*.css')
+        .pipe($.concat('main.css'))
+        .pipe($.cleanCss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('dist/css'))
 });
 
 // Minify custom JS
-gulp.task('minify-js', function() {
-  return gulp.src('js/*.js')
-    .pipe($.uglify())
-      .pipe($.rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('dist/js'))
+gulp.task('minify-js', function () {
+    return gulp.src('client/js/*.js')
+        .pipe($.uglify())
+        .pipe($.concat('bundle.js'))
+        .pipe(gulp.dest('dist/js'))
 });
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
+gulp.task('copy', function () {
+    gulp.src([
+        'node_modules/bootstrap/dist/**/*',
+        '!**/npm.js',
+        '!**/bootstrap-theme.*',
+        '!**/*.map'
     ])
-    .pipe(gulp.dest('dist/vendor/bootstrap'))
+        // .pipe(gulp.dest('dist/vendor/bootstrap'))
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('dist/vendor/jquery'))
+    // gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    //     .pipe(gulp.dest('dist/vendor/jquery'))
+    //
+    // gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    //     .pipe(gulp.dest('dist/vendor/popper'))
+    //
+    // gulp.src(['node_modules/jquery.easing/*.js'])
+    //     .pipe(gulp.dest('dist/vendor/jquery-easing'))
 
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('dist/vendor/popper'))
-
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('dist/vendor/jquery-easing'))
-
-  gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
+    gulp.src([
+        'node_modules/font-awesome/**',
+        '!node_modules/font-awesome/**/*.map',
+        '!node_modules/font-awesome/.npmignore',
+        '!node_modules/font-awesome/*.txt',
+        '!node_modules/font-awesome/*.md',
+        '!node_modules/font-awesome/*.json'
     ])
-    .pipe(gulp.dest('dist/vendor/font-awesome'))
+        .pipe(gulp.dest('dist/vendor/font-awesome'))
 
     gulp.src(['./imgs/**'])
         .pipe(gulp.dest('dist/imgs'))
@@ -89,7 +85,6 @@ gulp.task('copy', function() {
 })
 
 
-
 gulp.task('build', ['minify-css', 'minify-js', 'copy'], () => {
     return gulp.src('./client/index.html')
         .pipe($.htmlmin({
@@ -99,12 +94,11 @@ gulp.task('build', ['minify-css', 'minify-js', 'copy'], () => {
 });
 
 gulp.task('watch', () => {
-    return gulp.watch(['./index.html', './css/*.css'], ['build']);
+    return gulp.watch(['./client/index.html', './client/css/*.css'], ['build']);
 });
 
 // Default task
 gulp.task('default', ['build', 'watch']);
-
 
 
 // Dev task with browserSync
