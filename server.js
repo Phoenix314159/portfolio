d1d3c7496045c8d2e4492fd09ac6e5534851b35a
James Langford <derivative7117@live.com>
2017-11-02 23:20:56 -0600

    dude

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express'),
     app = express(),
     port = process.env.PORT || 3225;
 
-// app.use(express.static(__dirname + '/client'));
 
-//<------ production --------->
 
 process.env.PWD = process.cwd();
 
 app.use('/', express.static(process.env.PWD + '/dist'));
 
-//<--------------------------->
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
