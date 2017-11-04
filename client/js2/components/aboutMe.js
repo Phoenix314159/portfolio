eeea583a7ff2f503064b364dd5bb4170d3f90638
James Langford <derivative7117@live.com>
2017-11-04 15:53:31 -0600

    changed background pictures

--- client/js2/components/aboutMe.js
+++ client/js2/components/aboutMe.js
@@ -1,7 +1,7 @@
 angular.module('portfolio').component('aboutMe', {
   template: `<div class="container">
-              <div class="row">
-              <div class="col-lg-8 mx-auto">
+             <div class="row">
+             <div class="col-lg-8 mx-auto">
                 <h2 class="wow animated fadeIn">About </h2>
                 <hr class="projectsLine"/>
                 <p class="wow animated slideInLeft text45">{{$ctrl.aboutMeText1}}</p>
