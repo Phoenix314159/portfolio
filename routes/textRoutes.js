553571f9b40d35d418e1c2facdbc2ca8db11d694
James Langford <derivative7117@live.com>
2017-11-05 15:30:17 -0700

    added error handling on async routes

--- routes/textRoutes.js
+++ routes/textRoutes.js
@@ -1,17 +1,23 @@
-const mainCtrl = require('../controllers/mainCtrl')
+const asyncError = require('../middleware/catchErrors'),
+  {aboutMe1, aboutMe2, keyboards,
+    vimeo, solar, adventure,
+    toolbox, youtube, feedback,
+    weather, blogger} = require('../controllers/mainCtrl');
 
 module.exports = app => {
-  app.get('/api/about_me1', mainCtrl.aboutMe1)
-  app.get('/api/about_me2', mainCtrl.aboutMe2)
-  app.get('/api/keyboard_text', mainCtrl.keyboards)
-  app.get('/api/vimeo_text', mainCtrl.vimeo)
-  app.get('/api/solar_text', mainCtrl.solar)
-  app.get('/api/adventure_text', mainCtrl.adventure)
-  app.get('/api/toolbox_text', mainCtrl.toolbox)
-  app.get('/api/youtube_text', mainCtrl.youtube)
-  app.get('/api/feedback_text', mainCtrl.feedback)
-  app.get('/api/weather_text', mainCtrl.weather)
-  app.get('/api/blogger_text', mainCtrl.blogger)
+
+  app.get('/api/about_me1', asyncError(aboutMe1))
+  app.get('/api/about_me2', asyncError(aboutMe2))
+  app.get('/api/keyboard_text', asyncError(keyboards))
+  app.get('/api/vimeo_text', asyncError(vimeo))
+  app.get('/api/solar_text', asyncError(solar))
+  app.get('/api/adventure_text', asyncError(adventure))
+  app.get('/api/toolbox_text', asyncError(toolbox))
+  app.get('/api/youtube_text', asyncError(youtube))
+  app.get('/api/feedback_text', asyncError(feedback))
+  app.get('/api/weather_text', asyncError(weather))
+  app.get('/api/blogger_text', asyncError(blogger))
+
 }
 
 
